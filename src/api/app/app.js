f619077d4f196e4cadb9d6a22ec60bd39c527bea
alicedev94 <alicedev94@gmail.com>
2024-10-17 15:26:13 -0400

    web pushh added

--- src/api/app/app.js
+++ src/api/app/app.js
@@ -1,17 +1,42 @@
 const express = require("express");
+const webpush = require("web-push");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
 const path = require("path");
 
-const app = express();
+app.use(bodyParser.json());
 
-const port = 3000;
+// VAPID keys should be generated only once.
+// const vapidKeys = webpush.generateVAPIDKeys();
 
-app.use(express.static(path.join(__dirname, "../public")));
-console.log(path.join(__dirname, "../public"));
+webpush.setVapidDetails(
+  "mailto:test@example.com",
+  "BD_ZMjYJnK7XVLJYioT6mjqzWGZ31hDFK4y759zBDDXBw1qktWUaAf5VRtX3nQTcKIh74hbIIRz9c4mTD5kf6RQ", //vapidKeys.publicKey,
+  "b_vMob27tSX8Y1YGJEsq6CRC-wgV3lpku26o1GjLlew" //vapidKeys.privateKey
+);
 
-app.get("/", (req, res) => {
-  res.send("api run in port 3000");
+// console.log(vapidKeys)
+
+const publicRoute = path.join(__dirname, "../public");
+
+app.use(express.static(publicRoute));
+
+app.post("/api/subscription", (req, res) => {
+  const subscription = req.body;
+  res.status(201).json({});
+
+  const payload = JSON.stringify({
+    title: "Nueva notificación",
+    body: "Lorem ipsum dolor sit amet.",
+    icon: "/logo-deno-132x132.png",
+  });
+
+  webpush.sendNotification(subscription, payload).catch((error) => {
+    console.error("err", error.stack);
+  });
 });
 
 app.listen(port, () => {
-  console.log("Running in port: ", port);
+  console.log(`Server running on port ${port}`);
 });
